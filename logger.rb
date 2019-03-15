696b404b60c5c21ad762e9f40ba36732d2126afd
lomochka <yfgurda@gmail.com>
2019-03-15 23:13:08 +0200

    include Singleton

--- logger.rb
+++ logger.rb
@@ -1,17 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
-  end
-
-  @x = Logger.new
-  def self.instance
-    return @x
   end
 
   # instance nethod
   def log_something(wat)
     @f.puts(wat)
   end
-
-  private_class_method :new
 end
